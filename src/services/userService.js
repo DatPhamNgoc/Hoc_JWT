eaf273227e110e3cbbe2d85e173a83a7a32c1def
datphamngoc <ngocdat254@gmail.com>
2024-07-17 11:07:58 +0700

    step 3 API register & hash password

---
+++ src/services/userService.js
@@ -0,0 +1,29 @@
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const saltRounds =10;
+const createUserService = async (name, email, password) => {
+    try {
+        // hash user password
+        const hashPassword = await bcrypt.hash(password, saltRounds)
+        //save user to database
+        let result = await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: "user or admin"
+        })
+        return result;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+// function tao moi 1 nguoi dung
+
+
+
+
+module.exports = {
+    createUserService
+}
